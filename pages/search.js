09c8e20f184b1762a6ab4215cb1a4d4e95130d49
Roshis Rai <roshis.awai@gmail.com>
2025-04-08 01:59:11 -0400

    fix: JSX quotes escaping in search.js to pass Vercel deployment checks.
    - Properly escaped double quotes using &quot; in help text

--- pages/search.js
+++ pages/search.js
@@ -63,7 +63,7 @@ export default function AdvancedSearch() {
             <Form.Label>Geo Location</Form.Label>
             <Form.Control type="text" placeholder="" {...register("geoLocation")} />
             <Form.Text className="text-muted">
-              Case Sensitive String (ie "Europe", "France", "Paris", "China", "New York", etc.), with multiple values separated by the | operator
+            Case Sensitive String (ie &quot;Europe&quot;, &quot;France&quot;, &quot;Paris&quot;, &quot;China&quot;, &quot;New York&quot;, etc.), with multiple values separated by the | operator
             </Form.Text>
           </Form.Group>
         </Col>
@@ -72,7 +72,7 @@ export default function AdvancedSearch() {
             <Form.Label>Medium</Form.Label>
             <Form.Control type="text" placeholder="" {...register("medium")} />
             <Form.Text className="text-muted">
-              Case Sensitive String (ie: "Ceramics", "Furniture", "Paintings", "Sculpture", "Textiles", etc.), with multiple values separated by the | operator
+            Case Sensitive String (ie: &quot;Ceramics&quot;, &quot;Furniture&quot;, &quot;Paintings&quot;, &quot;Sculpture&quot;, &quot;Textiles&quot;, etc.), with multiple values separated by the | operator
             </Form.Text>
           </Form.Group>
         </Col>
